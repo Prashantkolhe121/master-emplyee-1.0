5e4c76f709e1d57f333ae0db5005baf733eb42e5
Prashantkolhe121 <prashantkolhe101@gmail.com>
2024-03-24 13:07:32 +0530

    Add files via upload
    First service

---
+++ RegistrationService/src/test/java/com/org/prash/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.prash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
