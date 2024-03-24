cab2a3116cbc810faf0f11f32a790c6a416fcfd1
Prashant Kolhe <prashantkolhe101@gmail.com>
2024-03-24 13:55:01 +0530

    First Commit

---
+++ RegistrationService/src/main/java/com/org/prash/RegistrationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.org.prash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
