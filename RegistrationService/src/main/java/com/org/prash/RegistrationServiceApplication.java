143f31f78dadbe004bf0f81fce5accb9f73faf35
Prashant Kolhe <prashantkolhe101@gmail.com>
2024-03-24 14:26:04 +0530

    file changes

--- RegistrationService/src/main/java/com/org/prash/RegistrationServiceApplication.java
+++ RegistrationService/src/main/java/com/org/prash/RegistrationServiceApplication.java
@@ -8,7 +8,7 @@ public class RegistrationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationServiceApplication.class, args);
-		System.out.println("hello");
+		System.out.println("hello Prashant");
 	}
 
 }
